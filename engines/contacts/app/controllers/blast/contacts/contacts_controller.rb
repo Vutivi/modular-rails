9212aee96a471fb1174074b5ab092dc1b2fc4b44
vutivi <vutivi.xanani@gmail.com>
2020-12-08 14:25:06 +0200

    chore: extend admin

--- engines/contacts/app/controllers/blast/contacts/contacts_controller.rb
+++ engines/contacts/app/controllers/blast/contacts/contacts_controller.rb
@@ -6,7 +6,7 @@ module Blast
       before_action :set_contact, only: [:show, :edit, :update, :destroy]
 
       def index
-        @contacts = Contact.all
+        @contacts = current_user.contacts
       end
 
       def show
